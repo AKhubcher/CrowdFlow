b18e7fee727719463a7469a384d2c904eb15b0ff
Arya Khubcher <ecstaticlimb@gmail.com>
2026-03-03 11:34:16 -0800

    add Vercel Analytics for page view tracking

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { Analytics } from '@vercel/analytics/react';
 import { AppRoutes } from './router';
 
 export default function App() {
-  return <AppRoutes />;
+  return (
+    <>
+      <AppRoutes />
+      <Analytics />
+    </>
+  );
 }
